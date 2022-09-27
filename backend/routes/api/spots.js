6afc002282b39c220f8607441f1880ffc1fb4c0a
evan <ebmorgansb@gmail.com>
2022-09-27 10:24:42 -0700

    Get all spots endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,15 +1,42 @@
 // backend/routes/api/index.js
 const express = require('express')
-const {Spot, Booking, Review, ReviewImage, SpotImage} = require('../../db/models')
+const {Spot, Booking, Review, ReviewImage, SpotImage} = require('../../db/models');
+const spot = require('../../db/models/spot');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     let allSpots = await Spot.findAll({
-    //   include: Review
+       include: [{model: Review}, {model: SpotImage}]
+    //   //  {model: SpotImage,
+    //   //   attributes: 'previewImage'
+    //   // }
+    // ]
     })
-    // for (let i = 0; i <allSpots.length; i++) {
-    //   let spot = allSpots[i]
-  res.json(allSpots);
+    let spotArr = []
+    for (let i = 0; i <allSpots.length; i++) {
+      let counter = 0
+      let spot = allSpots[i].toJSON()
+      spotArr.push(spot)
+      let reviews = spot.Reviews
+      let images = spot.SpotImages
+      for (let i = 0; i < reviews.length; i++) {
+        let review = reviews[i]
+        let starz = review.stars
+        counter += starz
+      }
+      for (let i = 0; i < images.length; i++) {
+        let image = images[i]
+        let url = image.url
+        spot.previewImage = url
+      }
+      delete spotArr[i].SpotImages
+      let avgRating = counter/reviews.length
+      spot.avgRating = avgRating
+      delete spotArr[i].Reviews
+
+    }
+
+  res.json(spotArr);
 });
 
 module.exports = router;
