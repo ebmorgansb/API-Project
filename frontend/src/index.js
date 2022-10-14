1adafa67da5bccfd7af45b9162bd928cc5c76460
evan <ebmorgansb@gmail.com>
2022-10-13 21:15:34 -0700

    setup csrf

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,18 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 
+// ... other imports
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+//
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
