c1372f7906d758d2914ecdb77cd47e7cb9d7124d
evan <ebmorgansb@gmail.com>
2022-10-17 11:15:59 -0700

    reducers

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,22 @@
 import SpotCard from './SpotCard';
 import SpotImages from './SpotImages'
+import {useDispatch, useSelector} from 'react-redux'
+import {useEffect} from 'react'
+import {fetchSpots} from '../store/spots'
 
 function Home () {
+  const dispatch = useDispatch()
+
+
+  const spotz = useSelector((state) => {
+    console.log(state)
+  })
+
+    useEffect(() => {
+      dispatch(fetchSpots())
+    }, [])
+
+
     return (
       <div>
         <SpotCard/>
