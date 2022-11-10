79ac157af199234a761d03c38bdf9a113f217566
evan <ebmorgansb@gmail.com>
2022-11-09 17:16:02 -0800

    added images folder

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,12 +1,28 @@
 import SpotCard from '../SpotCard/SpotCard';
 import './home.css'
+import gitLogo from '../../allImages/github-logo.png';
+import inLogo from '../../allImages/in.png';
 
 function Home () {
 
 console.log('hello')
     return (
-      <div>
+      <div className='homeDiv'>
         <SpotCard/>
+        <footer className='footer'>
+          <div>
+          An AirBnB clone by Evan Morgan
+          </div>
+          <div>
+          <a  target="_blank" href='https://github.com/ebmorgansb'>
+            <img className='gitLogo'  alt='githubLogo' src={gitLogo}></img>
+            
+          </a>
+          <a  target="_blank" href='https://www.linkedin.com/in/evan-morgan-9a2723132/'>
+            <img className='inLogo' alt='inLogo' src={inLogo}></img>
+          </a>
+          </div>
+        </footer>
       </div>
     );
   }
